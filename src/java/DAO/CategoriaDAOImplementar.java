0bad90d234b6005a82db789dd80c90c90dca0e34
Ernesto Acevedo <natbeat2016@gmail.com>
2020-06-14 22:50:02 -0600

    Se añadieron las funcionalidades, y carpeta Bootstrap

--- src/java/DAO/CategoriaDAOImplementar.java
+++ src/java/DAO/CategoriaDAOImplementar.java
@@ -1,30 +1,32 @@
 package DAO;
 
 import Model.Categoria;
-import java.util.List;
 import Factory.ConexionBD;
 import Factory.FactoryConexionDB;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CategoriaDAOImplementar implements CategoriaDAO{
+public class CategoriaDAOImplementar implements CategoriaDAO {
+
     ConexionBD conn; //Crear el objeto tipo conexion
-    
-    public CategoriaDAOImplementar(){
+
+    public CategoriaDAOImplementar() {
         //Definir a la base de datos que se conectara por defecto
-        this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
+
     }
-    
+
     @Override
     public List<Categoria> Listar() {
+        this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
         StringBuilder miSQL = new StringBuilder();//Construir consulta
         miSQL.append("SELECT * FROM tb_categoria;");//Agregar consulta
         List<Categoria> lista = new ArrayList<Categoria>();
         try {
             //Se crear el objeto ResultSet implementando el metodo(consultaSQL) creado para la consulta
             ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
-            while(resultadoSQL.next()){
+            while (resultadoSQL.next()) {
                 Categoria categoria = new Categoria();//Declarar el objeto categoria
                 //Asignar cada campo consultado al atributo en la clase
                 categoria.setId_categoria(resultadoSQL.getInt("id_categoria"));
@@ -32,8 +34,8 @@ public class CategoriaDAOImplementar implements CategoriaDAO{
                 categoria.setEstado_categoria(resultadoSQL.getInt("estado_categoria"));
                 lista.add(categoria);//Agregar al array cada registro encontrado
             }
-        } catch (Exception ex) {
-        }finally{
+        } catch (SQLException ex) {
+        } finally {
             this.conn.cerrarConexion();//Para cerrar la conexion
         }
         return lista;
@@ -41,22 +43,75 @@ public class CategoriaDAOImplementar implements CategoriaDAO{
 
     @Override
     public List<Categoria> Listar2() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public Categoria editarCat(int id_cat_edit) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        
+        Categoria categoria = new Categoria();//Objeto categoria para devolver datos
+        StringBuilder miSQL = new StringBuilder();//Construir la consulta
+        //Agregar la consulta
+        miSQL.append("SELECT * FROM tb_categoria WHERE id_categoria =").append(id_cat_edit);
+        try {
+            ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
+            while (resultadoSQL.next()) {
+                categoria.setId_categoria(resultadoSQL.getInt("id_categoria"));
+                categoria.setNom_categoria(resultadoSQL.getString("nom_categoria"));
+                categoria.setEstado_categoria(resultadoSQL.getInt("estado_categoria"));
+            }
+        } catch (SQLException ex) {
+
+        } finally {
+            this.conn.cerrarConexion();//Cerrar conexion
+        }
+        return categoria;
     }
 
     @Override
     public boolean guardarCat(Categoria categoria) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);//Hacer la conexion
+        boolean guarda = false;//Bandeja de resultado
+        try {
+            if (categoria.getId_categoria() ==0) {//Para cuando es una nueva categoria
+                StringBuilder miSQL = new StringBuilder();
+                //Agregar la consulta SQL; el id es autoincrementable
+                miSQL.append("INSERT INTO tb_categoria (nom_categoria, estado_categoria) VALUES('");
+                miSQL.append(categoria.getNom_categoria() + "', ").append(categoria.getEstado_categoria());
+                miSQL.append(");");
+                //  Invocar metodo para ejecutar la consulta
+                this.conn.ejecutarSQL(miSQL.toString());
+            }else if(categoria.getId_categoria() > 0){ //Actualizar, id_categoria mayores a 0
+                StringBuilder miSQL = new StringBuilder();
+                miSQL.append("UPDATE tb_categoria SET id_categoria = ").append(categoria.getId_categoria());
+                miSQL.append(", nom_categoria = '").append(categoria.getNom_categoria());
+                miSQL.append("', estado_categoria = ").append(categoria.getEstado_categoria());
+                miSQL.append(" WHERE id_categoria = ").append(categoria.getId_categoria()).append(";");
+                //Invocar metodo para ejecutar la consulta
+                this.conn.ejecutarSQL(miSQL.toString());
+            }
+            guarda = true;
+        } catch (Exception e) {
+        }finally{
+            this.conn.cerrarConexion();
+        }
+        return guarda;
     }
 
     @Override
     public boolean borrarCat(int id_cat_borrar) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);//Hacer la conexion
+        boolean borra = false; //Bandera de resultado
+        try {
+            StringBuilder miSQL = new StringBuilder();
+            miSQL.append("DELETE FROM tb_categoria WHERE id_categoria = ").append(id_cat_borrar);
+            this.conn.ejecutarSQL(miSQL.toString());
+            borra = true;
+        } catch (Exception e) {
+        }finally{
+            this.conn.cerrarConexion();//Cerrar la conexion
+        }
+        return borra;
     }
-    
+
 }
--- src/java/Test/Prueba.java
+++ src/java/Test/Prueba.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class Prueba {
     public static void main(String[] args) {
         Prueba evaluar = new Prueba();
-        evaluar.listarCategorias();
+        evaluar.listarCategorias();//Metodo listar antes de eliminaar
+        /*evaluar.eliminarCategoria();*///Metodo que eliminara la categoria indicada
+        evaluar.listarCategorias();//Consultar si ha sido eliminada la categoria
     }
     
     public void listarCategorias(){
@@ -21,4 +23,37 @@ public class Prueba {
                                " ESTADO: "  + categoriaListar.getEstado_categoria());
         }
     }
+    
+     public void editarCategoria(){
+        CategoriaDAO categoria = new CategoriaDAOImplementar();
+        Categoria cat_edit = categoria.editarCat(1);
+         System.out.println("CATEGORIA A MODIFICAR");
+         System.out.println("ID: " + cat_edit.getId_categoria() +
+                            " NOMBRE: " + cat_edit.getNom_categoria()+
+                            " ESTADO: "  + cat_edit.getEstado_categoria());
+    }
+     
+    
+     public void guardaCategoria(){
+         CategoriaDAO categoria = new CategoriaDAOImplementar();
+         Categoria guarda_cat = new Categoria();
+         guarda_cat.setNom_categoria("Bebidas Naturales");//Nueva categoria a guardar
+         guarda_cat.setId_categoria(5);//Modificar la categoria registrada anteriormente
+         guarda_cat.setEstado_categoria(1);//Estado 1
+         categoria.guardarCat(guarda_cat);
+     }
+     
+    /*public void guardaCategoria(){
+         CategoriaDAO categoria = new CategoriaDAOImplementar();
+         Categoria guarda_cat = new Categoria();
+         guarda_cat.setNom_categoria("Bebidas");//Nueva categoria a guardar
+         guarda_cat.setEstado_categoria(1);//Estado 1
+         categoria.guardarCat(guarda_cat);
+     }*/
+     
+     public void eliminarCategoria(){
+         CategoriaDAO categoria = new CategoriaDAOImplementar();
+         categoria.borrarCat(6);//Se eliminara la categoria con id_Categoria = 6
+     }
+        
 }
